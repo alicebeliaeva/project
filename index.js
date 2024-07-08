4476653ae042aedb576de339da49510356dd884a
alicebeliaeva <138302299+alicebeliaeva@users.noreply.github.com>
2024-07-08 21:21:54 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,175 @@
+"use strict"
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// const obj = {
+//     a: 50
+// }
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// const vehicleBodyWidth =5000;
+// const vehicleBodyLength =4000;
+
+// console.log("width of body:" + vehicleBodyWidth + ', length: ' + vehicleBodyLength);
+
+// ['vnvnv', 'bvbvb', 'kvkvk'].map(a => 1);
+
+// request
+// data
+// response 
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+// const COLOR_RED = '#F00';
+
+// let number = 4.6;
+
+// HTMLFormControlsCollection.log(4/0);
+// console.log('string'*9);
+
+// const persone ='Alex';
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "john",
+//     age: 25, 
+//     isMarried: false
+// };
+// console.log(obj.name);
+
+// массив не являеться отдельным типом данных, а частным случаем объекта
+
+// let arr = ['plum.png','orange.pnj', 6, {}, []]; 
+// console.log(arr[1]);
+
+// const arr = ['a','b','c'];
+
+// arr[10] =3456;
+
+// console.log(arr[10]);
+
+// const arrObj = {
+//     0:'a',
+//     1:'b',
+//     2:'c',
+//     abc: {
+//         df: [{},{}],
+//         def:{
+
+//         }
+//     }
+// };
+// const b = 'b'
+// arrObj[b] = '1234';
+
+// console.log (arrObj ['b']);
+
+// console.log (arrObj.b);
+
+// // const obj = (a: 1, b: 2);
+// const obj = {
+//     'anna': 500,
+//     'alice': 800
+// };
+
+// 12 lesson !!!!!
+// alert('Hiiii');
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answer = +prompt("are you 18?", "42");
+// console.log(answer +5 );
+
+// all the information comming from the user will always be string
+
+// const answers = [];
+
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your surname?','');
+// answers[2] = prompt('how old are you?', '');
+
+// console.log(typeof(answers));
+
+//13 урок, интерполяция
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Oleg";
+
+// alert(`hello, ${user}`);
+
+// const storeName = 'store';
+// const storeDescription = {
+//     'buget': 10000,
+//     'employees':['oleg','oleg'],
+//     'roducts': {
+//         'price1':700,
+//         'price2':200,
+//     },
+
+//     'open':true
+// }
+
+//14 урок,операторы 
+// console.log('arr'+" - object");
+// console.log(4 +" - object");
+// console.log(4 + +"5"); //унарный плюс
+
+//инкримент,дикремет
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === '8'); //false
+
+// const isChecked = true,
+//       isClosed = true;
+// console.log(isChecked && isClosed);
+
+// const isChecked = true,
+//       isClosed = false;
+// console.log(isChecked || isClosed);
+
+// const isChecked = false,
+//       isClosed = false;
+// console.log(isChecked || !isClosed);
+
+// console.log(5%2);
+
+console.log(2+2*2 != 8); 
